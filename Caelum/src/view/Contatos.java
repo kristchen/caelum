430556c59bdf9b36c3f134856ee8257fedb04f4e
halex <halex@localhost>
2014-08-16 16:47:03 -0300

    Mais 1

---
+++ Caelum/src/view/Contatos.java
@@ -0,0 +1,44 @@
+package view;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.ContatoDAO;
+import beans.Contato;
+
+public class Contatos extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		Contato contato = new Contato();
+
+		PrintWriter out = response.getWriter();
+
+		String nome = request.getParameter("nome");
+		String endereco = request.getParameter("endereco");
+		String email = request.getParameter("email");
+		String dataNascimento = request.getParameter("dataNascimento");
+
+		contato.setNome(nome);
+		contato.setEmail(email);
+		contato.setEndereco(endereco);
+		contato.setDataNascimento(dataNascimento);
+
+		new ContatoDAO().salvar(contato);
+		
+		
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Contato " + contato.getNome() + " adicionado com sucesso");
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+}
